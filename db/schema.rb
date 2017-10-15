817beb1ff01d7ca48373dea47814e701996f958f
Ali <30610753+aamining@users.noreply.github.com>
2017-10-15 12:26:35 +1100

    first commit

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,19 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170117022338) do
+ActiveRecord::Schema.define(version: 20171015005144) do
+
+  create_table "profiles", force: :cascade do |t|
+    t.string   "name"
+    t.string   "username"
+    t.integer  "age"
+    t.string   "gender"
+    t.integer  "user_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.string   "picture"
+    t.index ["user_id"], name: "index_profiles_on_user_id"
+  end
 
   create_table "users", force: :cascade do |t|
     t.string   "email",                  default: "", null: false
