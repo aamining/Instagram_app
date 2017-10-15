d17493afd31ea21b4ecfb33b4f0e7b752cd747a7
Ali <30610753+aamining@users.noreply.github.com>
2017-10-15 12:59:45 +1100

    add post table

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,4 +1,5 @@
 class HomeController < ApplicationController
   def page
+    @posts = Post.all
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,18 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171015005144) do
+ActiveRecord::Schema.define(version: 20171015014440) do
+
+  create_table "posts", force: :cascade do |t|
+    t.text     "status"
+    t.integer  "user_id"
+    t.integer  "profile_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.string   "image"
+    t.index ["profile_id"], name: "index_posts_on_profile_id"
+    t.index ["user_id"], name: "index_posts_on_user_id"
+  end
 
   create_table "profiles", force: :cascade do |t|
     t.string   "name"
